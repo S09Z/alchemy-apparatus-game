d17cd21b1a6af5bee551aadf314878abf9429cf1
Ittichai Boonyarakthunya <i.boonyarakthunya@gmail.com>
2026-06-30 15:23:42 +0700

    feat: add IngredientData autoload with 7 ingredient definitions

---
+++ godot/scripts/data/IngredientData.gd
@@ -0,0 +1,11 @@
+extends Node
+
+const INGREDIENTS: Dictionary = {
+	"water":          { "name": "Water",          "type": "base",    "color": Color(0.4, 0.6, 0.9) },
+	"mint_herb":      { "name": "Mint Herb",       "type": "powder",  "color": Color(0.3, 0.8, 0.4) },
+	"golden_essence": { "name": "Golden Essence",  "type": "liquid",  "color": Color(0.9, 0.75, 0.2) },
+	"iron_dust":      { "name": "Iron Dust",       "type": "powder",  "color": Color(0.5, 0.5, 0.6) },
+	"crimson_sap":    { "name": "Crimson Sap",     "type": "liquid",  "color": Color(0.8, 0.2, 0.2) },
+	"azure_crystal":  { "name": "Azure Crystal",   "type": "powder",  "color": Color(0.3, 0.5, 0.9) },
+	"moon_dew":       { "name": "Moon Dew",        "type": "liquid",  "color": Color(0.7, 0.8, 1.0) },
+}
